a8a046ea0593c4ee0d77a4ddd7bb54408bc2046d
Jamie Ahn <jamie50240002@gmail.com>
2024-10-16 09:50:42 +0000

    10.16 - 1

--- src/components/organisms/SideContents.tsx
+++ src/components/organisms/SideContents.tsx
@@ -1,12 +1,9 @@
 export default function SideContents() {
   return (
     <div className="flex flex-col w-1/5 gap-2">
-      <div className="flex items-center justify-center p-5 bg-white rounded-lg h-36">
-        <img src="/images/profile.png" alt="" />
-      </div>
-      <div className="flex items-center justify-center p-5 bg-white rounded-lg h-36">
-        <img src="/images/profile.png" alt="" />
-      </div>
+    <div className="flex items-center justify-center p-5 bg-white rounded-lg h-36">
+      <img src="./images/profile.png" alt="" />
     </div>
+  </div>
   );
 }
