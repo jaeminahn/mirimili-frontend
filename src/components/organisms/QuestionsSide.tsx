6a3339cce10ccef022088acfa1668d527c20519f
jaeminahn <jamie50240002@gmail.com>
2025-09-03 10:05:21 +0000

    QuestionsSide

--- src/components/organisms/QuestionsSide.tsx
+++ src/components/organisms/QuestionsSide.tsx
@@ -5,6 +5,22 @@ export default function QuestionsSide() {
   return (
     <>
       <div className="hidden lg:flex flex-col lg:w-1/5 gap-4">
+        <div className="flex flex-col gap-3 p-6 bg-white rounded-3xl">
+          <p className="text-sm font-bold">내 활동</p>
+          <div className="justify-center gap-2 text-xs">
+            <div className="flex justify-center gap-3 p-2 bg-gray-100 rounded-lg">
+              <div className="flex gap-1">
+                <p>질문</p>
+                <p className="font-semibold">5</p>
+              </div>
+              <div className="flex gap-1">
+                <p>답변</p>
+                <p className="font-semibold">2</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <Link to="/questions/new" className="hidden lg:block">
           <div className="flex items-center justify-center h-12 gap-2 font-semibold text-white rounded-3xl bg-emerald-600 hover:bg-emerald-700">
             <p>질문하기</p>
